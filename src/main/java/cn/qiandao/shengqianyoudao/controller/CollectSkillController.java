80aa8e28b6cd5c9c886f1149e95271fb182269b1
lxy0512 <lxy9291214@163.com>
2020-02-06 16:38:03 +0800

    增加收藏的api注解

--- src/main/java/cn/qiandao/shengqianyoudao/controller/CollectSkillController.java
+++ src/main/java/cn/qiandao/shengqianyoudao/controller/CollectSkillController.java
@@ -3,6 +3,10 @@ package cn.qiandao.shengqianyoudao.controller;
 import cn.qiandao.shengqianyoudao.pojo.Skilluserrelationship;
 import cn.qiandao.shengqianyoudao.service.ISkillRelationService;
 import cn.qiandao.shengqianyoudao.service.RedisService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +20,8 @@ import javax.annotation.Resource;
 @RestController
 @Slf4j
 @RequestMapping("/CollectSkill")
+@Api(value="/CollectSkill",description="收藏")
+@CrossOrigin
 public class CollectSkillController {
 
     @Autowired
@@ -23,16 +29,14 @@ public class CollectSkillController {
     @Resource
     private RedisService redisService;
 
-    @GetMapping("/getUser/{uId}")
-    public Skilluserrelationship selUser(@PathVariable("uId") String skillsId) {
-        return service.selUser(skillsId);
-    }
-
-
-
     /**
      * 点赞
      */
+    @ApiOperation(value="用户点赞收藏",notes="用户点赞收藏")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="skillsId",value="技能编号"),
+            @ApiImplicitParam(name="userId",value="用户编号")
+    })
     @PostMapping("/{skillsId}/{userId}")
     public Object likeArticle(@PathVariable("skillsId") String skillsId,
                               @PathVariable("userId") String uId) {
@@ -43,6 +47,11 @@ public class CollectSkillController {
     /**
      * 取消点赞
      */
+    @ApiOperation(value="用户取消点赞收藏",notes="用户取消点赞收藏")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="skillsId",value="技能编号"),
+            @ApiImplicitParam(name="userId",value="用户编号")
+    })
     @DeleteMapping("/{skillsId}/{userId}")
     public Object unlikeArticle(@PathVariable("skillsId") String skillsId,
                                 @PathVariable("userId") String userId) {

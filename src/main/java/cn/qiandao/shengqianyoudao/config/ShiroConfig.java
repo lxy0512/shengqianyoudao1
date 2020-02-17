6051b3a071ca970842261c2902be5d95f17f9f41
jazz-KDA <56617879+jazz-KDA@users.noreply.github.com>
2020-02-17 17:24:40 +0800

    Add files via upload

---
+++ src/main/java/cn/qiandao/shengqianyoudao/config/ShiroConfig.java
@@ -0,0 +1,81 @@
+package cn.qiandao.shengqianyoudao.config;
+
+import cn.qiandao.shengqianyoudao.realm.ShiroRealm;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @Description
+ * @Author wt
+ * @data
+ */
+@Configuration
+public class ShiroConfig {
+
+    @Bean
+    public SecurityManager securityManager(){
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        //将自定义的realm交给SecurityManager管理
+        securityManager.setRealm(new ShiroRealm());
+        return securityManager;
+    }
+
+    /**
+     * 开启aop注解支持
+     * 即在controller中使用 @RequiresPermissions("user/userList")
+     */
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager){
+        AuthorizationAttributeSourceAdvisor attributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        //设置安全管理器
+        attributeSourceAdvisor.setSecurityManager(securityManager);
+        return attributeSourceAdvisor;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator defaultAAP = new DefaultAdvisorAutoProxyCreator();
+        defaultAAP.setProxyTargetClass(true);
+        return defaultAAP;
+    }
+
+    /**
+     * 配置Shiro的Web过滤器，拦截浏览器请求并交给SecurityManager处理
+     * @return
+     */
+    @Bean
+    public ShiroFilterFactoryBean webFilter(SecurityManager securityManager){
+        System.out.println("shiro拦截");
+
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        //设置securityManager
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+
+        //配置拦截链 使用LinkedHashMap,因为LinkedHashMap是有序的，shiro会根据添加的顺序进行拦截
+        // Map<K,V> K指的是拦截的url V值的是该url是否拦截
+        Map<String,String> filterChainMap = new LinkedHashMap<String,String>(16);
+        //配置退出过滤器logout，由shiro实现
+        filterChainMap.put("/logout","logout");
+        //authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问,先配置anon再配置authc。
+        filterChainMap.put("/**","anon");
+
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainMap);
+        return shiroFilterFactoryBean;
+    }
+
+
+}
